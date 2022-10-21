df437a6cfdf96e8c44361c39418148405ed292c7
Bode <jry.bode@gmail.com>
2022-10-21 22:41:11 +0700

    login

---
+++ login-form.php
@@ -0,0 +1,10 @@
+<?php include "connect.php"?>
+<html>
+    <body>
+        <form action="login-check.php" method="post">
+        Username: <input type="text" value=""><br>
+        Password: <input type="type" value=""><br>
+        <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
