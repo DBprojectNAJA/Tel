6b1c828f44432c093dc2781ae7724352247894ac
nut2334 <nut2334@gmail.com>
2022-11-04 00:27:21 +0700

    ไม่แก้ละ ลบเลยละกัน

--- Register/insert-new-customer.php
+++ Register/insert-new-customer.php
@@ -1,14 +1,13 @@
-<?php include "../connect.php" ?>
+<?php include "../connect.php"?>
 
 <?php 
-
 $stmt = $pdo->prepare("INSERT INTO customer VALUES ( ?, ?, ?)");
 $stmt->bindParam(1, $_POST["cus_name"]);
 $stmt->bindParam(2, $_POST["cus_prefix"]);
 $stmt->bindParam(3, $_POST["cus_tel"]);
-
 $stmt->execute();
-header( "refresh:2;url=../search/search.php" );
+
+header('refresh:2;url=../search/search.php');
 ?>
 
 <html>
