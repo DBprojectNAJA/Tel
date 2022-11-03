2331f0f8fd4dba0f7c8e800849d7f4fe9e9b3ed3
nut2334 <nut2334@gmail.com>
2022-11-03 18:20:05 +0700

    center

--- Login/login-form.php
+++ Login/login-form.php
@@ -1,13 +1,15 @@
 <?php include "../connect.php"?>
 <html>
     <head>
-    <?php include "../nav/nav_login.php" ?>
+        <?php include "../nav/nav_login.php" ?>
     </head>
     <body>
-        <form action="login-check.php" method="post">
-        Username: <input type="text" name="employee_id"><br>
-        Password: <input type="password" name="emp_tel"><br>
-        <input type="submit" value="Login">
-        </form>
+        <center>
+            <form action="login-check.php" method="post">
+            Username: <input type="text" name="employee_id"><br>
+            Password: <input type="password" name="emp_tel"><br>
+            <input type="submit" value="Login">
+            </form>
+        </center>
     </body>
 </html>
