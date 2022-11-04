776e27a0faa5f5813039975bd9edd032193d9b1a
Bode <jry.bode@gmail.com>
2022-11-04 15:19:21 +0700

    ตกแต่งหน้าเพิ่มเครื่อง(เบื้องต้น)

--- Phone/insert-phone.php
+++ Phone/insert-phone.php
@@ -2,12 +2,53 @@
 
 ?>
 <html>
-    <body>
-    <form action="insert-phone-to-database.php?cus_name=<?=$_GET['cus_name'] ?>" method="post">
-        tel_model: <input type="text" name="tel_model"><br>
-        color: <input type="text" name="color"><br>
-        <input type="submit" value="add">
+    <head>
+    <mega charset="utf-8">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <style>
+        * {
+                font-family: 'Prompt';
+            }
+        h1{
+            text-align: center;
+            font-weight: bold;
+        }
+
+        .add{display: block;
+            margin-bottom: 50px;
+            justify-content: center;
+            background-color:white;
+            margin: 15vh 20% 20vh 20%;
+            padding: 5%;
+            border-radius: 10px;
+            /* text-align: center; */
+        }
+        body {
+                margin: 0px auto 0px auto;
+                background-color: #7FB5FF;
+            }
+        .main{
+            margin: 130px 370px 193.8px;
+            position: static;
+            left: 0;
+            right: 0;
+        }
+    </style>
+    </head>
+    <body>
+
+    <?php include "../nav/nav_login.php" ?>
+    <?php include "../nav/nav.php" ?>
+    <h1> เพิ่มเครื่องเข้าระบบ </h1>
+    <div class="main">
+        <div class="add">
+        <form action="insert-phone-to-database.php?cus_name=<?=$_GET['cus_name'] ?>" method="post">
+            tel_model: <input type="text" name="tel_model"><br>
+            color: <input type="text" name="color"><br>
+            <input type="submit" value="add">
+    </div>        
         </form>
+    </div>      
 </body>
 </html>        
