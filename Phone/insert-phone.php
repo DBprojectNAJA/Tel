7ac5180a9a58ba099c1b9c3313f982e16897e2eb
nut2334 <nut2334@gmail.com>
2022-11-19 00:36:23 +0700

    css

--- Phone/insert-phone.php
+++ Phone/insert-phone.php
@@ -6,36 +6,7 @@
     <mega charset="utf-8">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <style>
-        * {
-                font-family: 'Prompt';
-            }
-        h1{
-            text-align: center;
-            font-weight: bold;
-            font-size: 72px;
-        }
-
-        .add{display: block;
-            margin-bottom: 50px;
-            justify-content: center;
-            background-color:white;
-            margin: 15vh 20% 20vh 20%;
-            padding: 5%;
-            border-radius: 10px;
-            /* text-align: center; */
-        }
-        body {
-                margin: 0px auto 0px auto;
-                background-color: #7FB5FF;
-            }
-        .main{
-            margin: 130px 370px 193.8px;
-            position: static;
-            left: 0;
-            right: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/insert-phone.css">
     </head>
     <body>
 
