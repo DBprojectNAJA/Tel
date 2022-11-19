d9085ab85a5cfc680b7ea75ee1bb1223113718e4
Nattavee <s6302062620096@email.kmutnb.ac.th>
2022-11-19 22:30:08 +0700

    ลบ comment ใน contact ออก

--- contact/contact.php
+++ contact/contact.php
@@ -10,7 +10,7 @@
 .then(data => {
     let objectData = data.data
     for (let i = 0; i < objectData.length; i++) {
-            let branch = objectData[i].branch // ดึงข้อมูลจาก object
+            let branch = objectData[i].branch
             let address = objectData[i].address
             let map = objectData[i].map
             let tel = objectData[i].tel
@@ -33,35 +33,6 @@
             tr.appendChild(td4)
             }
 })
-            // let rawData = await response.text() 
-            // objectData = JSON.parse(rawData) // แปลผลลัพธ์เป็น object
-            
-            // let result = document.getElementById('result') // ดึง <ul> เพื่อใช้ในการเพิ่มแท็ก <li>มาจัดรูปแบบ
-
-            // for (let i = 0; i < objectData.data.length; i++) {
-            // let branch = objectData.data[i].branch // ดึงข้อมูลจาก object
-            // let address = objectData.data[i].address
-            // let map = objectData.data[i].map
-            // let tel = objectData.data[i].tel
-
-            // let Table = document.getElementById('tab')
-            // let tr = document.createElement('tr')
-            // Table.appendChild('tr')
-
-            // let td1 = document.createElement('td')
-            // td1.innerHTML = branch
-            // let td2 = document.createElement('td')
-            // td2.innerHTML = address
-            // let td3 = document.createElement('td')
-            // td3.innerHTML = map
-            // let td4 = document.createElement('td')
-            // td4.innerHTML = tel
-
-            // tr.appendChild(td1)
-            // tr.appendChild(td2)
-            // tr.appendChild(td3)
-            // tr.appendChild(td4)
-            // }
             }
             getDataFromAPI()
         </script>
