d90296c2387052dbe0e69c217ce0731a2b2d0bae
nut2334 <nut2334@gmail.com>
2022-11-19 23:20:39 +0700

    aaa

--- nav/nav.php
+++ nav/nav.php
@@ -4,6 +4,7 @@
 
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Mali&family=Prompt:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         * {
             font-family: 'Prompt';
